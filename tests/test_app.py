86640e3ce1e9b500b19fc24730c035ed88a49fd5
PedroPinheiro <henrique363@gmail.com>
2024-01-19 14:57:53 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,18 @@
-def test_root_deve_retornar_200_e_ola_mundo(client):
+from fastapi.testclient import TestClient
 
+from fast_zero.app import app
+from fast_zero.schemas import UserPublic
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_200_e_ola_mundo():
     response = client.get('/')
-
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
 def test_create_user(client):
-
     response = client.post(
         '/users/',
         json={
@@ -27,20 +32,19 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
@@ -55,8 +59,22 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
-    response = client.delete('/users/1')
-
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
