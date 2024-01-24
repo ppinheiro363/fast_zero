775c4a442eb834511e63aca43490beab08841cf2
PedroPinheiro <henrique363@gmail.com>
2024-01-24 10:17:37 -0300

    Dockerizando nossa aplicação e alterando os testes para serem executados no PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,25 @@
-import factory
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
 from fast_zero.database import get_session
-from fast_zero.models import Base, User
+from fast_zero.models import Base
+from fast_zero.settings import Settings
 from fast_zero.security import get_password_hash
-
-
-class UserFactory(factory.Factory):
-    class Meta:
-        model = User
-
-    id = factory.Sequence(lambda n: n)
-    username = factory.LazyAttribute(lambda obj: f'test{obj.id}')
-    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
-    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
+from tests.factories import UserFactory
 
 
 @pytest.fixture
 def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
+    engine = create_engine(Settings().DATABASE_URL)
     Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base.metadata.create_all(engine)
-    yield Session()
+    with Session() as session:
+        yield session
+        session.rollback()
+
     Base.metadata.drop_all(engine)
 
 
@@ -50,6 +39,7 @@ def client(session):
 def user(session):
     password = 'testtest'
     user = UserFactory(password=get_password_hash(password))
+
     session.add(user)
     session.commit()
     session.refresh(user)
@@ -60,9 +50,9 @@ def user(session):
 
 
 @pytest.fixture
-def other_user(session):
+def user2(session):
     password = 'testtest'
-    user = UserFactory(password=get_password_hash(password))
+    user = UserFactory(id=2, password=get_password_hash(password))
 
     session.add(user)
     session.commit()
